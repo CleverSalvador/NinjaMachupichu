944fc479058cb44ef131fd7d1567dbd916a766fb
CleverSalvador <85046870+CleverSalvador@users.noreply.github.com>
2022-06-22 20:51:46 -0500

    cofre, checkpoint, palanca

---
+++ Assets/Script/Interactuable.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactuable : MonoBehaviour
+{
+    private bool puedeInteractuar;
+    private BoxCollider2D bc;
+    private SpriteRenderer sp;
+    private GameObject indicarInteractuable;
+    private Animator anim;
+    public GameObject[] objetos;
+    public bool esCofre;
+    //Palanca
+    public UnityEvent evento;
+    public bool esPalanca;
+    public bool palancaAccionada;
+
+
+    private void Awake() 
+    {
+        bc = GetComponent<BoxCollider2D>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if(transform.GetChild(0) != null){
+            indicarInteractuable = transform.GetChild(0).gameObject;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.CompareTag("Player"))
+        {
+            puedeInteractuar = true;
+            indicarInteractuable.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.CompareTag("Player"))
+        {
+            puedeInteractuar = false;
+            indicarInteractuable.SetActive(false);
+        }
+    }
+
+    private void Cofre()
+    {
+        if(esCofre)
+        {
+            Instantiate(objetos[Random.Range(0, objetos.Length)], transform.position, Quaternion.identity);
+        }
+        anim.SetBool("abrir", true);
+        bc.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(puedeInteractuar && Input.GetKeyDown(KeyCode.C))
+        {
+            Cofre();
+            Palanca();
+        }
+    }
+
+    /*Metodo en caso sea la Palanca*/
+    private void Palanca()
+    {
+        if(esPalanca && !palancaAccionada)
+        {
+            anim.SetBool("activar", true);
+            palancaAccionada = true;
+            evento.Invoke();
+            indicarInteractuable.SetActive(false);
+            bc.enabled = false;
+            this.enabled = false;
+        }
+        
+    }
+}
